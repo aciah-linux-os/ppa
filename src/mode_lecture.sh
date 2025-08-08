ec735db6dab6039fda3e0e03cc7959b23e0a6921
ACIAH <116746145+aciah-linux-os@users.noreply.github.com>
2025-08-08 15:11:19 +0200

    Creation mode_lecture.sh
    passer un article en mode lecture avec firefox - touche F9

---
+++ src/mode_lecture.sh
@@ -0,0 +1,14 @@
+!/bin/sh
+# VERSION 1.0
+# Auteur : association ACIAH
+# Licence : GPL - v3
+# Modifications :
+# Nom : Mode lecture
+# DESCRIPTION : passer un article en mode lecture avec firefox - touche F9 
+# DEPENDANCES : le script nécessite le paquet xdotool 
+# COMPLEMENTS : 
+# RACCOURCIS : ce script est placé dans le dossier /usr/local/bin
+#
+sleep 1
+xdotool key "Ctrl+Alt+r"
+exit 0
