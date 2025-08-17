0759b262a2da602907ef73fc5324a57b6bfa7009
ACIAH <116746145+aciah-linux-os@users.noreply.github.com>
2025-08-17 21:15:47 +0200

    Mise à jour mode_lecture.sh

--- src/mode_lecture.sh
+++ src/mode_lecture.sh
@@ -8,7 +8,21 @@
 # DEPENDANCES : le script nécessite le paquet xdotool 
 # COMPLEMENTS : 
 # RACCOURCIS : ce script est placé dans le dossier /usr/local/bin
-#
-sleep 1
-xdotool key "Ctrl+Alt+r"
-exit 0
+
+STATE_FILE="/tmp/mode_lecture_state"
+
+ # Petite pause pour laisser Firefox réagir
+    sleep 1
+
+ # Simulation de Ctrl+Alt+R
+    xdotool key "Ctrl+Alt+r"
+
+    if [ ! -f "$STATE_FILE" ]; then
+        # Pas encore en mode lecture → on active
+        espeak -a 200 -v mb-fr1 -s 130 "passage en mode lecture"
+        touch "$STATE_FILE"
+    else
+        # Déjà en mode lecture → on désactive
+        espeak -a 200 -v mb-fr1 -s 130 "mode lecture terminé"
+        rm -f "$STATE_FILE"
+    fi
