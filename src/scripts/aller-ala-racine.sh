e3395af69cc1cd27d47962d9998d68657170dcbb
ACIAH <116746145+aciah-linux-os@users.noreply.github.com>
2026-03-18 10:53:39 +0100

    Créer aller-ala-racine.sh
    une seule touche pour aller à la racine de l'arborescence, à affecter à la touche F1, script destiné aux personnes qui n'ont qu'une main.

---
+++ src/scripts/aller-ala-racine.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# VERSION 1.0
+# Auteur : association ACIAH
+# Licence : GPL - v3
+# Modifications :
+# Nom : aller-ala-racine
+# DESCRIPTION : une seule touche pour aller à la racine de l'arborescence, à affecter à la touche F1, script destiné aux personnes qui n'ont qu'une main.
+# DEPENDANCES : le script nécessite le paquet  xdotool
+# COMPLEMENTS : 
+# RACCOURCIS : ce script est placé dans le dossier /usr/local/bin
+#
+sleep 0.5 && xdotool key Alt+Home
+exit 0
