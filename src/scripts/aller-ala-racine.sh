7428c1e3ba408d86e4ccccafdb6e88ad64547e1d
ACIAH <116746145+aciah-linux-os@users.noreply.github.com>
2026-03-18 10:57:08 +0100

    mise à jour aller-ala-racine.sh
    Ceci remplace le raccourci Alt+Home

--- src/scripts/aller-ala-racine.sh
+++ src/scripts/aller-ala-racine.sh
@@ -4,7 +4,7 @@
 # Licence : GPL - v3
 # Modifications :
 # Nom : aller-ala-racine
-# DESCRIPTION : une seule touche pour aller à la racine de l'arborescence, à affecter à la touche F1, script destiné aux personnes qui n'ont qu'une main.
+# DESCRIPTION : une seule touche pour aller à la racine de l'arborescence, script destiné aux personnes qui n'ont qu'une main.
 # DEPENDANCES : le script nécessite le paquet  xdotool
 # COMPLEMENTS : 
 # RACCOURCIS : ce script est placé dans le dossier /usr/local/bin
